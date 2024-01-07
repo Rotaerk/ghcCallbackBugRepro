bef12c466614f7194643dcdac2c134d768223d68
rotaerk <m.scott.stewart@gmail.com>
2024-01-07 17:33:55 -0500

    Reproduces bug now.

--- csrc/callme.c
+++ csrc/callme.c
@@ -1,8 +1,13 @@
+#include <stddef.h>
 #include <stdint.h>
 
-typedef uint64_t (*CallbackPtr)(uint64_t p1, uint64_t p2, uint64_t p3, uint64_t p4, uint64_t p5, uint64_t p6, uint64_t p7, uint64_t p8);
+typedef void (*PtrAll64)(uint64_t p1, uint64_t p2, uint64_t p3, uint64_t p4, uint64_t p5, uint64_t p6, uint64_t p7, uint64_t p8);
+typedef void (*PtrOne32)(uint64_t p1, uint64_t p2, uint32_t p3, uint64_t p4, uint64_t p5, uint64_t p6, uint64_t p7, uint64_t p8);
+typedef void (*PtrTwo32)(uint64_t p1, uint32_t p2, uint32_t p3, uint64_t p4, uint64_t p5, uint64_t p6, uint64_t p7, uint64_t p8);
 
-uint64_t callMe(CallbackPtr callbackPtr)
+void callMe(PtrAll64 ptrAll64, PtrOne32 ptrOne32, PtrTwo32 ptrTwo32)
 {
-  return (*callbackPtr)(1,2,3,4,5,6,7,8);
+  (*ptrAll64)(1,2,3,4,5,6,7,8);
+  (*ptrOne32)(1,2,3,4,5,6,7,8);
+  (*ptrTwo32)(1,2,3,4,5,6,7,8);
 }
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import Data.Int
 import Data.Word
+import Foreign.C.String
 import Foreign.Ptr
 
 main :: IO ()
 main = do
-  callbackPtr <- wrapCallback $ \p1 p2 p3 p4 p5 p6 p7 p8 -> do
+  all64Ptr <- wrapAll64 $ \p1 p2 p3 p4 p5 p6 p7 p8 -> do
+    putStrLn "Callback with only 64-bit parameters..."
     putStrLn $ "P1: " ++ show p1
     putStrLn $ "P2: " ++ show p2
     putStrLn $ "P3: " ++ show p3
@@ -14,12 +17,37 @@ main = do
     putStrLn $ "P6: " ++ show p6
     putStrLn $ "P7: " ++ show p7
     putStrLn $ "P8: " ++ show p8
-    return (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8)
-  result <- callMe callbackPtr
-  putStrLn $ "Result: " ++ show result
+  one32Ptr <- wrapOne32 $ \p1 p2 p3 p4 p5 p6 p7 p8 -> do
+    putStrLn "Callback with one 32-bit parameter and the rest 64-bit..."
+    putStrLn $ "P1: " ++ show p1
+    putStrLn $ "P2: " ++ show p2
+    putStrLn $ "P3: " ++ show p3
+    putStrLn $ "P4: " ++ show p4
+    putStrLn $ "P5: " ++ show p5
+    putStrLn $ "P6: " ++ show p6
+    putStrLn $ "P7: " ++ show p7
+    putStrLn $ "P8: " ++ show p8
+  two32Ptr <- wrapTwo32 $ \p1 p2 p3 p4 p5 p6 p7 p8 -> do
+    putStrLn "Callback with two 32-bit parameters and the rest 64-bit..."
+    putStrLn $ "P1: " ++ show p1
+    putStrLn $ "P2: " ++ show p2
+    putStrLn $ "P3: " ++ show p3
+    putStrLn $ "P4: " ++ show p4
+    putStrLn $ "P5: " ++ show p5
+    putStrLn $ "P6: " ++ show p6
+    putStrLn $ "P7: " ++ show p7
+    putStrLn $ "P8: " ++ show p8
+  callMe all64Ptr one32Ptr two32Ptr
+  freeHaskellFunPtr all64Ptr
+  freeHaskellFunPtr one32Ptr
+  freeHaskellFunPtr two32Ptr
 
 type DynamicWrapper f = f -> IO (FunPtr f)
-type Callback = Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> IO Word64
+type All64 = Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> IO ()
+type One32 = Word64 -> Word64 -> Word32 -> Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> IO ()
+type Two32 = Word64 -> Word32 -> Word32 -> Word64 -> Word64 -> Word64 -> Word64 -> Word64 -> IO ()
 
-foreign import ccall "callMe" callMe :: FunPtr Callback -> IO Word64
-foreign import ccall "wrapper" wrapCallback :: DynamicWrapper Callback
+foreign import ccall "callMe" callMe :: FunPtr All64 -> FunPtr One32 -> FunPtr Two32 -> IO ()
+foreign import ccall "wrapper" wrapAll64 :: DynamicWrapper All64
+foreign import ccall "wrapper" wrapOne32 :: DynamicWrapper One32
+foreign import ccall "wrapper" wrapTwo32 :: DynamicWrapper Two32
